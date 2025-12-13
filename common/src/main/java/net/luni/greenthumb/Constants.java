2d167da99916c37dfc7499e70bcd645c974cb760
luni <mgmt@mrrp.dev>
2025-12-14 03:02:06 +0900

    Initial Commit

---
+++ common/src/main/java/net/luni/greenthumb/Constants.java
@@ -0,0 +1,11 @@
+package net.luni.greenthumb;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Constants {
+
+    public static final String MOD_ID = "green_thumb";
+    public static final String MOD_NAME = "green_thumb";
+    public static final Logger LOG = LoggerFactory.getLogger(MOD_NAME);
+}
